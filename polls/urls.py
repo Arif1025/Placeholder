98fd543c11d4b6cb8582392bff1a112b550f3002
K23070652 <K23070652@kcl.ac.uk>
2025-02-03 14:41:32 +0000

    Login Page with urls and login redirect logic

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from .views import login_view
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("login/", login_view, name="login"),
+    path("student-dashboard/", student_dashboard, name="student_dashboard"),
+    path("professor-dashboard/", professor_dashboard, name="professor_dashboard"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
+from .forms import CustomLoginForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return HttpResponse("Hello, this is the index view.")
 
 def homepage(request):
     return HttpResponse("Hello! This is the homepage at the root URL.")
+
+def login_view(request):
+    if request.method == "POST":
+        form = CustomLoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            role = form.cleaned_data.get("role")
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None and user.role == role:
+                login(request, user)
+                if role == "professor":
+                    return redirect("professor_dashboard")
+                return redirect("student_dashboard")
+    
+    else:
+        form = CustomLoginForm()
+    
+    return render(request, "polls/login.html", {"form": form})
+
+@login_required
+def student_dashboard(request):
+    return render(request, "polls/student_dashboard.html")
+
+@login_required
+def professor_dashboard(request):
+    return render(request, "polls/professor_dashboard.html")
