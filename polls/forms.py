7e107e6ed407e93c94934019115a43bfc358483e
jbedi21 <21.jbedi@gmail.com>
2025-02-22 15:10:05 +0000

    Merge question_template to main

---
+++ polls/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class CustomLoginForm(AuthenticationForm):
+    role = forms.ChoiceField(
+        choices=[('student', 'Student'), ('professor', 'Professor')],
+        required=True,
+        label="Login as"
+    )
+    
