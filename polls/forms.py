4d0268236d7cfda5a5b5e8ad1d63086517cc5f6f
Khuslen Bambar <khuslenbambar191@gmail.com>
2025-02-27 12:36:18 +0000

    quick fix in forms.py

--- polls/forms.py
+++ polls/forms.py
@@ -1,28 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
-from .models import Poll, Question
-
-class PollForm(forms.ModelForm):
-    class Meta:
-        model = Poll
-        fields = ['title', 'description']
-
-class QuestionForm(forms.ModelForm):
-    class Meta:
-        model = Question
-        fields = ['question_text']
-
-class TeacherRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ["username", "email", "password1", "password2"]
-
-class TeacherLoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+from django.contrib.auth.forms import AuthenticationForm
 
 class CustomLoginForm(AuthenticationForm):
     role = forms.ChoiceField(
