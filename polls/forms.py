6a38fb571a29609e8af01fb29d875ac8f8840cb3
K23070652 <K23070652@kcl.ac.uk>
2025-03-07 14:39:42 +0000

    add question button now adds each question as a new poll to the database. Also renamed quiz_creation.html to create_quiz.html

--- polls/forms.py
+++ polls/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.forms import inlineformset_factory
 from django.contrib.auth.forms import AuthenticationForm
 from .models import Question
-from .models import Poll
+from .models import Poll, Question, Choice
 
 class CustomLoginForm(AuthenticationForm):
     role = forms.ChoiceField(
@@ -36,3 +37,11 @@ class QuestionForm(forms.ModelForm):
     text = forms.CharField(label="Question Text", widget=forms.Textarea)
     question_type = forms.ChoiceField(choices=[('text', 'Written Answer'), ('mcq', 'Multiple Choice')])
     options = forms.CharField(required=False, help_text=OPTIONS_HELP_TEXT)
+
+class ChoiceForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        fields = ['choice_text']
+
+QuestionFormSet = inlineformset_factory(Poll, Question, form=QuestionForm, extra=1, can_delete=True)
+ChoiceFormSet = inlineformset_factory(Question, Choice, form=ChoiceForm, extra=3, can_delete=True)
--- polls/views.py
+++ polls/views.py
@@ -3,9 +3,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .forms import CustomLoginForm, QuestionForm, PollForm
+from .forms import CustomLoginForm, QuestionFormSet, PollForm
 from django.forms import modelformset_factory
-from .models import Poll, Question, CustomUser
+from .models import Poll, Question, Choice, CustomUser
 
 def index(request):
     return HttpResponse("Hello, this is the index view.")
@@ -40,10 +40,10 @@ def student_home_interface(request):
 
 @login_required
 def teacher_home_interface(request):
-    polls = Poll.objects.filter(created_by=request.user)
-    return render(request, "teacher_home_interface.html")
+    quizzes = Poll.objects.all()
+    return render(request, "teacher_home_interface.html", {"polls": quizzes})
 
-
+"""
 def create_quiz(request):
     # Initialize the Poll form
     poll_form = PollForm()
@@ -58,8 +58,14 @@ def create_quiz(request):
         poll_form = PollForm(request.POST)
         question_formset = QuestionFormSet(request.POST)
 
+        if "add_question" in request.POST:
+            question_formset.extra += 1
+            return render(request, "quiz_creation.html", {
+                "poll_form": poll_form,
+                "question_formset": question_formset
+            })
+
         if poll_form.is_valid() and question_formset.is_valid():
-            # Save the poll (but don't commit to DB yet to add the user)
             poll = poll_form.save(commit=False)
             poll.created_by = request.user  # Set the user who created the poll
             poll.save()  # Now save the poll to the database
@@ -70,13 +76,41 @@ def create_quiz(request):
                 question.poll = poll  # Link each question to the poll
                 question.save()  # Save the question
 
-            return redirect("teacher_home_interface")  # Redirect to the teacher home interface
+            return redirect("teacher_home_interface")
 
     # If it's not a POST request, just render the formset and poll form
     return render(request, "quiz_creation.html", {
         "poll_form": poll_form,
         "question_formset": question_formset
     })
+"""
+@login_required
+def create_quiz(request):
+    if request.method == "POST":
+        form = PollForm(request.POST)
+        #formset = QuestionFormSet(request.POST)
+        
+        if form.is_valid(): #and formset.is_valid():
+            quiz = form.save(commit=False)
+            quiz.created_by = request.user
+            quiz.save()
+            #questions = formset.save(commit=False)
+
+            questions_data = request.POST.getlist("questions")  
+            for question_text in questions_data:
+                question = Question(poll=quiz, text=question_text)
+                question.save()
+                #formset.save_m2m()
+
+            return redirect('teacher_home_interface')  # Redirect to teacher home after saving
+        else:
+            print(form.errors)  #For debugging
+            #print(formset.errors)  #For debugging
+    else:
+        form = PollForm()
+        #formset = QuestionFormSet()
+
+    return render(request, 'create_quiz.html', {'form': form})#, 'formset': formset})
 
 def logout_view(request):
     logout(request)  # Log out the user
