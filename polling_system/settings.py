5b69f566731d177dd7c5408d2d6a19b913520b62
rebornakalay <chuhao.weng@kcl.ac.uk>
2025-03-26 15:52:25 +0000

    polling_result.html password_reset_confirm.html

--- polling_system/settings.py
+++ polling_system/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+EMAIL_BACKEND ='django.core.mail.backends.smpt.EmailBackend'
+EMAIL_HOST = 'smpt.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TIS = True
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = 'gmailpassword'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
